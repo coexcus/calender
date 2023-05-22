5366ac0215a7d99dd1df9b826d08387945e54de3
sksar <soumyakantisar@gmail.com>
2023-05-22 21:47:07 +0530

    Updates

--- src/extract.js
+++ src/extract.js
@@ -1,26 +1,25 @@
 // noinspection SpellCheckingInspection
 
 export default function (data) {
-
-    const EVENTS = data['VCALENDAR'][0]['VEVENT'];
-
-    return EVENTS.map(E => {
-
-        const start = parseDate(E['DTSTART;VALUE=DATE']);
-        const end = parseDate(E['DTEND;VALUE=DATE']);
-
-        const summary = E['SUMMARY'];
-        const location = E['LOCATION'];
-
-        return { start, end, summary, location }
-
-    });
+    const rawEvents = data['VCALENDAR'][0]['VEVENT'];
+    return rawEvents
+        .map(E => {
+            const start = parseDate(E['DTSTART;VALUE=DATE']);
+            const end = parseDate(E['DTEND;VALUE=DATE']);
+            const summary = E['SUMMARY'];
+            const location = E['LOCATION'];
+            return {start, end, summary, location}
+        })
+        .filter(filter)
+        .sort((a, b) => a.start.localeCompare(b.start));
 }
 
 function parseDate(date) {
     const d = date.split("T")[0];
-    const year = d.slice(0, 4);
-    const month = d.slice(4, 6);
-    const day = d.slice(6, 8);
-    return `${year}-${month}-${day}`;
+    return `${d.slice(0, 4)}-${d.slice(4, 6)}-${d.slice(6, 8)}`;
+}
+
+function filter(e) {
+    const timestamp = new Date(e.end).valueOf();
+    return timestamp >= Date.now() - 30 * 864e5; // 30 days
 }
